a83ea860c073ca3c409c8a0643647b5b1707f6b5
Silent <joe25142021@gmail.com>
2024-12-10 15:07:51 +0800

    重新設定新的耶利哥晨禱的report

--- report_redirection/index.js
+++ report_redirection/index.js
@@ -24,7 +24,7 @@ liff.init({
             console.log(data);
             let form = document.createElement('form');
             form.method = 'post';
-            form.action = "https://script.google.com/macros/s/AKfycbx9Aha_85wswzTlYZ1xETh9OSaUnYrtIFKjhcaqekhtlAdMIaYdLbugabZbT3FcV65k/exec"
+            form.action = "https://script.google.com/macros/s/AKfycbxOhI3ND1b8TgmndXdHrf6cwTzYSioUHOXG-3WkrUP2f0_aBWCz2h8TTCvKb10d1vlkjg/exec"
             document.body.appendChild(form);
     
             for (const key in data) {
